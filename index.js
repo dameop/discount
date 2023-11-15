a347553edae91a7b4ece9c945556aa31c4d03e32
dameop <150899769+dameop@users.noreply.github.com>
2023-11-15 11:04:01 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,85 @@
+odule.exports = [
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'BENJAMINMP',
+    offer: '30% discount'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'HKLOVE',
+    offer: '50% off'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'MPJJANI',
+    offer: '22% off'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'HK40',
+    offer: '40% discount'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'ALISSA20',
+    offer: '20% off'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'MRSGRONO20',
+    offer: '20% off'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'PATRICK70',
+    offer: '10% off'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'MARCELLMYP',
+    offer: '40% off'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'QUICK40',
+    offer: '20% off'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'EAST37',
+    offer: '37% off'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'ZERMATT',
+    offer: '47% off'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'JITTERHAPS',
+    offer: '47% off'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'BDAY50',
+    offer: '50% off'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'GOLD50',
+    offer: '50% off'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'SYLVESTER',
+    offer: '55% Off'
+  },
+  {
+    site: 'https://emporium.com.ua/',
+    discount_code: 'HKGYM42',
+    offer: '42% off'
+  }
+  
+    offer: '45% off'
+  }
+]
